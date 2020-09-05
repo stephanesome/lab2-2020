f44a09db7d03546e4fc5ccf08db213c28b45ce2a
Stéphane Somé <stephanesome@gmail.com>
2020-09-04 23:28:04 -0400

    Lab2 complete

---
+++ src/app/address-list/address-list-element/address-list-element.component.ts
@@ -0,0 +1,32 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {AddressEntry} from '../address-entry';
+import {NotificationService} from '../notification.service';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-address-list-element',
+  templateUrl: './address-list-element.component.html',
+  styleUrls: ['./address-list-element.component.css']
+})
+export class AddressListElementComponent implements OnInit, OnDestroy {
+  @Input() address: AddressEntry;
+  selected = false;
+  subscription: Subscription;
+
+  constructor(private notificationService: NotificationService) {
+    this.subscription = notificationService.selectedElement.subscribe(newAddress => {
+      this.selected = newAddress === this.address ? true : false;
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  getFullName(): string {
+    return `${this.address.firstName}, ${this.address.lastName}`;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
